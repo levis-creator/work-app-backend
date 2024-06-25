649506745a3b1f8dd0855fe7b5c0114680876190
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-25 04:28:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,59 @@
 import express from "express";
 const app =express();
+let products=[
+    {
+        id:1,
+        name:'product 1',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ut, deserunt ratione laborum quibusdam nesciunt nihil hic aut! Veritatis hic corrupti similique veniam delectus, neque necessitatibus id cupiditate suscipit a!"
+    },
+    {
+        id:2,
+        name:'product 2',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ut, deserunt ratione laborum quibusdam nesciunt nihil hic aut! Veritatis hic corrupti similique veniam delectus, neque necessitatibus id cupiditate suscipit a!"
+    },
+    {
+        id:3,
+        name:'product 3',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ut, deserunt ratione laborum quibusdam nesciunt nihil hic aut! Veritatis hic corrupti similique veniam delectus, neque necessitatibus id cupiditate suscipit a!"
+    },
+    {
+        id:4,
+        name:'product 4',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ut, deserunt ratione laborum quibusdam nesciunt nihil hic aut! Veritatis hic corrupti similique veniam delectus, neque necessitatibus id cupiditate suscipit a!"
+    },
+    {
+        id:5,
+        name:'product 5',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ut, deserunt ratione laborum quibusdam nesciunt nihil hic aut! Veritatis hic corrupti similique veniam delectus, neque necessitatibus id cupiditate suscipit a!"
+    },
+]
+// middleware
+app.use(express.json())
 
-app.get("/", (req, res)=>{
-    res.send("<h1>Hello there</h1>")
+// request
+
+// feching all data
+app.get("/products", (req, res)=>{
+    res.json(products)
+})
+// adding data to database
+app.post("/products", (req, res)=>{
+    const data = req.body
+    products.push(data)
+    return res.status(201).json(data)
+})
+// single data
+app.get('/products/:id', (req, res)=>{
+    const {id}=req.params
+    const singleData= products.filter((product)=>product.id==id)
+    return res.json(singleData[0]).status(200)
+})
+// delete data
+app.delete('/products/:id', (req, res)=>{
+    const {id }=req.params
+    const filteredData=products.filter((product)=>product.id!=id)
+    products=filteredData
+    return res.status(204).json(products)
 })
 
 
